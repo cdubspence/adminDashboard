040bb9f5a7f70b2bdbf5ba073f8abc051c48ac9e
Casey <orangobang@gmail.com>
2022-07-22 13:51:43 -0400

    buttons styled, sidemenu icons, links added

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const project = document.createElement('div');
 function addProject() {
     const title = prompt('Project Name: ');
     const description = prompt('Project Description: ');
+    if (title == '') {
+        return
+    }
     project.innerHTML = `<h3>${title}</h3>
     <p>${description}</p>`;
     project.className = 'card'
